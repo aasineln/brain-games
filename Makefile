fe3308e6237d91d8e0d895c665ecf962dd5c1583
a.sinelnikov <a.sinelnikov@inspector-cloud.com>
2026-02-21 00:19:27 +0300

    make brain even game

--- Makefile
+++ Makefile
@@ -4,8 +4,14 @@ install:
 brain-games:
 	uv run brain-games
 
+brain-even:
+	uv run brain-games
+
 build:
 	uv build
 
 package-install:
 	uv tool install dist/*.whl
+
+lint:
+	uv run ruff check brain_games
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,13 @@
-def say_hello():
+from brain_games.cli import welcome_user
+
+
+def greeting() -> None:
     print("Welcome to the Brain Games!")
 
 
-def main():
-    say_hello()
+def main() -> None:
+    greeting()
+    welcome_user()
 
 
 if __name__ == "__main__":
